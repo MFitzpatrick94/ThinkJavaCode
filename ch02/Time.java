26caa12777e68909e137bc680da99b21a30ca14b
MFitzpatrick94 <rasberrysoc@hotmail.com>
2018-04-18 12:27:16 -0500

    Completed Assignments

--- ch02/Time.java
+++ ch02/Time.java
@@ -5,17 +5,19 @@ public class Time
         double hour = 16.0;
         double minute = 24.0;
         double second = 10.0;
-        second =(hour*3600.0) + (minute*60.0) + second; //converted hours and minutes into seconds
-        System.out.println("Number of seconds since midnight: " + second);
+        double secondsSince =(hour*3600.0) + (minute*60.0) + second; //converted hours and minutes into seconds
+        System.out.println("Number of seconds since midnight: " + secondsSince);
 
-        second = (24.0*3600.00) - second;
-        System.out.println( "Number of seconds until midnight: " + second);
+        double secondsUntil = (24.0*3600.00) - secondsSince;
+        System.out.println( "Number of seconds until midnight: " + secondsUntil);
 
         double day = hour + (minute/60.0);
         System.out.println("Percentage of day passed: " + ( day / 24.00 ) * 100.00);
 
-
-        double start = (hour + (minute/60.0)) / ((36.0/60.0) + 15.0);
+        double x = 15.0;  //start hour
+        double y = 36.0; //start minute
+        //double z = 30.0; //start second
+        double start = (hour + (minute/60.0)) - ( x + (y/60.0));
         System.out.println( "Elapsed Time: " + start);
 
     }
