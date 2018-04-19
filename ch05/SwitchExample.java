0fe1d72665cac66e80b1962c9d143ffe51aea816
MFitzpatrick94 <rasberrysoc@hotmail.com>
2018-04-19 15:30:07 -0500

    Complete Assignments

---
+++ ch05/SwitchExample.java
@@ -0,0 +1,20 @@
+public class SwitchExample
+{
+    public static void main(String[] args)
+    {
+        lastNamewinner("Smith");
+    }
+
+    private static void lastNamewinner(String name)
+    {
+       /* switch (name)
+        {
+            case "Smith":
+            case "Jones":
+                name = System.out.println("Congratulations, grand winner");
+                break;
+        }
+*/
+
+    }
+}
